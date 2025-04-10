a75a5dd737a47d1b3f9c1b35c79d63afa4b5c2f5
celsadaka <cjs19@mail.aub.edu>
2025-04-10 13:15:59 +0300

    Update moves.c

--- moves.c
+++ moves.c
@@ -1,36 +1,42 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "board.h"
 
-void moves(char player)
-{
-    // to connect a line we need 2 dots
-    // row1 column1 dot row2 column2 second (4,0) (4,1)
-    int row1;
-    int row2;
-    int column1;
-    int column2;
-    printf("%c's turn. Enter the row and the column of the first dot (e.g., A0 -> 0 0) and the second dot: ", player);
-    scanf("%d %d %d %d", &row1, &column1, &row2, &column2);
-    // this takes the input from the user
+void moves(char player, int *row1, int *col1, int *row2, int *col2) {
+    printf("%c's turn. Enter the row and the column of the first dot (e.g., 0 0 0 1): ", player);
+    scanf("%d %d %d %d", row1, col1, row2, col2);
 
-    if (row1 == row2 && (column1 + 1 == column2 || column2 + 1 == column1)) {
-        int min_col = (column1 < column2) ? column1 : column2;
-        if (row1 >= 0 && row1 <= ROWS && min_col >= 0 && min_col < COLS && horizontal[row1][min_col] == ' ') {
-            horizontal[row1][min_col] = '-';
+    if (*row1 == *row2 && (*col1 + 1 == *col2 || *col2 + 1 == *col1)) {
+        int min_col = (*col1 < *col2) ? *col1 : *col2;
+        if (*row1 >= 0 && *row1 <= ROWS && min_col >= 0 && min_col < COLS && horizontal[*row1][min_col] == ' ') {
+            horizontal[*row1][min_col] = '-';
         } else {
             printf("Invalid move. Try again.\n");
-            moves(player);
+            moves(player, row1, col1, row2, col2);
         }
-    } else if (column1 == column2 && (row1 + 1 == row2 || row2 + 1 == row1)) {
-        int min_row = (row1 < row2) ? row1 : row2;
-        if (min_row >= 0 && min_row < ROWS && column1 >= 0 && column1 <= COLS && vertical[min_row][column1] == ' ') {
-            vertical[min_row][column1] = '|';
+    } 
+    else if (*col1 == *col2 && (*row1 + 1 == *row2 || *row2 + 1 == *row1)) {
+        int min_row = (*row1 < *row2) ? *row1 : *row2;
+        if (min_row >= 0 && min_row < ROWS && *col1 >= 0 && *col1 <= COLS && vertical[min_row][*col1] == ' ') {
+            vertical[min_row][*col1] = '|';
         } else {
             printf("Invalid move. Try again.\n");
-            moves(player);
+            moves(player, row1, col1, row2, col2);
         }
-    } else {
+    } 
+    else {
         printf("Invalid move. Try again.\n");
-        moves(player);
+        moves(player, row1, col1, row2, col2);
     }
+}
+
+bool isValidMove(int row1, int col1, int row2, int col2) {
+    if (row1 == row2) { // Horizontal line
+        int min_col = (col1 < col2) ? col1 : col2;
+        return row1 >= 0 && row1 <= ROWS && min_col >= 0 && min_col < COLS && horizontal[row1][min_col] == ' ';
+    } else if (col1 == col2) { // Vertical line
+        int min_row = (row1 < row2) ? row1 : row2;
+        return col1 >= 0 && col1 <= COLS && min_row >= 0 && min_row < ROWS && vertical[min_row][col1] == ' ';
+    }
+    return false; // Invalid move
 }
