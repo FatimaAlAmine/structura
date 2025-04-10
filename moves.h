a03a91cf8b137e056e62a30db96db707de370f3c
celsadaka <cjs19@mail.aub.edu>
2025-04-10 13:15:28 +0300

    Update moves.h

--- moves.h
+++ moves.h
@@ -1,6 +1,7 @@
 #ifndef MOVES_H
 #define MOVES_H
 
-void moves(char player);
+void moves(char player, int *row1, int *col1, int *row2, int *col2);
+bool isValidMove(int row1, int col1, int row2, int col2);
 
 #endif
